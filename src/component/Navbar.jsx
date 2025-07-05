3da4943eb5af7fc251960ff5b482cd6d95af6e70
JakeNLelis <jake.lelis05@gmail.com>
2025-07-05 17:23:26 +0800

    feat: implement Navbar component with user authentication and profile handling

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import ProfileImage from "./ProfileImage";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const {
+    id: userId,
+    token,
+    profilePhoto,
+  } = useSelector((state) => state.user?.currentUser);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);
+
+  return (
+    <nav className="navbar">
+      <div className="container navbar__container">
+        <Link to="/" className="navbar__logo">
+          Simphy
+        </Link>
+        <form className="navbar__search">
+          <input type="search" name="search" placeholder="Search" />
+          <button type="submit">
+            <CiSearch />
+          </button>
+        </form>
+        <div className="navbar__right">
+          <Link to={`/users/${userId}`} className="navbar__profile">
+            <ProfileImage image={profilePhoto} />
+          </Link>
+          {token ? (
+            <Link to="/logout" className="navbar__logout">
+              Logout
+            </Link>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { userActions } from "../store/user-slice";
 
 function Logout() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(userActions.changeCurrentUser({}));
+    localStorage.setItem("currentUser", JSON.stringify({}));
+    navigate("/login");
+  }, [dispatch, navigate]);
   return <div>Logout</div>;
 }
 
