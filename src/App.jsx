e56ea6ff89d2d771d2543e2fc32640ea9edcfd34
JakeNLelis <jake.lelis05@gmail.com>
2025-07-05 15:06:42 +0800

    feat: wrap App component with Redux Provider for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
 
 import RootLayout from "./RootLayout";
 import MessageList from "./component/MessageList";
@@ -12,26 +11,23 @@ import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import Register from "./pages/Register";
 import Bookmarks from "./pages/Bookmarks";
-import store from "./store/store";
 
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
-            <Route index element={<Home />} />
-            <Route path="messages" element={<MessageList />} />
-            <Route path="messages/:receiverId" element={<Messages />} />
-            <Route path="bookmarks" element={<Bookmarks />} />
-            <Route path="users/:id" element={<Profile />} />
-            <Route path="posts/:id" element={<SingePost />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Provider>
+      <Routes>
+        <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+          <Route index element={<Home />} />
+          <Route path="messages" element={<MessageList />} />
+          <Route path="messages/:receiverId" element={<Messages />} />
+          <Route path="bookmarks" element={<Bookmarks />} />
+          <Route path="users/:id" element={<Profile />} />
+          <Route path="posts/:id" element={<SingePost />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   );
 }
